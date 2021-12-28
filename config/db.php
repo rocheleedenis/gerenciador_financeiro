82c4bf5571d754fbbd6b6f4bc1b1c110153be18a
Rochele Edenís <rochele.edenis@gmail.com>
2021-12-27 23:27:14 -0300

    02 - Integrating with phinx

---
+++ config/db.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'development' => [
+        'driver' => 'mysql',
+        'host' => 'localhost',
+        'database' => 'curso_gerenciador_financeiro',
+        'username' => 'root',
+        'password' => '',
+        'charset' => 'utf8',
+        'collation' => 'utf8_unicode_ci',
+    ]
+];
