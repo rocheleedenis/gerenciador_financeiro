872b777f6195cf3837042e90e202313ab2cc924b
Rochele Edenís <rochele.edenis@gmail.com>
2021-12-30 12:23:56 -0300

    04 - Creation of the application class

---
+++ src/Application.php
@@ -0,0 +1,27 @@
+<?php
+
+use SonFin\ServiceContainerInterface;
+
+class Application
+{
+    private $serviceContainer;
+
+    public function __construction(ServiceContainerInterface $serviceContainer)
+    {
+        $this->serviceContainer = $serviceContainer;
+    }
+
+    public function service($name)
+    {
+        return $this->serviceContainer->get($name);
+    }
+
+    public function addService(string $name, $service)
+    {
+        if (is_callable($service)) {
+            $this->serviceContainer->addLazy($name, $service);
+        } else {
+            $this->serviceContainer->add($name, $service);
+        }
+    }
+}
