40119a514a1dd4ea79e2e29ebdeca09c01c2a5c4
Rochele Edenís <rochele.edenis@gmail.com>
2021-12-30 15:37:08 -0300

    07 - Configuring route combiner

--- src/Plugins/RoutePlugin.php
+++ src/Plugins/RoutePlugin.php
@@ -3,6 +3,8 @@
 use SonFin\Plugins\PluginInterface;
 use Aura\Router\RouterContainer;
 use SonFin\ServiceContainerInterface;
+use Psr\Http\Message\RequestInterface;
+use Zend\Diactoros\ServerRequestFactory;
 
 class RoutePlugin implements PluginInterface
 {
@@ -16,8 +18,25 @@ class RoutePlugin implements PluginInterface
         /* Tem a função de gerar links com base nas rotas registradas */
         $generator = $routeContainer->getGenerator();
 
+        $request = $this->getRequest();
+
         $container->add('routing', $map);
         $container->add('routing.matcher', $matcher);
         $container->add('routing.generator', $generator);
+        $container->add(RequestInterface::class, $request);
+
+        $container->addLazy('route', function (ContainerInterface $container) {
+            $matcher = $container->get('route.matcher');
+            $request = $container->get(RequestInterface::class);
+
+            return $matcher->match($request);
+        });
+    }
+
+    protected function getRequest(): RequestInterface
+    {
+        return ServerRequestFactory::fromGlobals(
+            $_SERVER, $_GET, $_POST, $_COOKIE, $_FILES
+        );
     }
 }
